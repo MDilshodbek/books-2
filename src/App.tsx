6a70325f8f1354024d18e90e1ec465bf7e4f15d7
MDilshodbek <mdbek55@gmail.com>
2024-09-09 20:47:15 +0900

    feat: trying to fix issue when committing!

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,9 @@
 import "./index.css";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
-import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <div>
-      <AlertDialog>
-        <AlertDialogTrigger>
-          <Button>Hi</Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction>Continue</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-      <div className="w-[1000px] h-[500px] bg-red-500 flex items-center justify-center m-auto">
-        <Carousel className="w-full p-[40px]">
-          <CarouselContent>
-            <CarouselItem>abc</CarouselItem>
-            <CarouselItem>def</CarouselItem>
-            <CarouselItem>ghi</CarouselItem>
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
-      </div>
+      <h1>Hello</h1>
     </div>
   );
 }
